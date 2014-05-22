25f4c279f82c245c0c246751cdaf13bd7ad404be
JayK31 <jay.kaye31@gmail.com>
2014-05-22 16:32:25 -0400

    added bootbox for dialog boxes

--- js/main.js
+++ js/main.js
@@ -7,16 +7,24 @@ $(document).ready(function() {
   });
   
   $("#tripcollab").click(function() {
-    alert("TripCollaborator is a way to plan trips and create a list of items to bring on your trips. You can invite guests, and they can choose to bring items on the trip. Built using Ruby on Rails, Twitter API, and RSpec.")
+    bootbox.dialog({
+      title: "TripCollaborator",
+      message: "TripCollaborator is a way to plan trips and create a list of items to bring on your trips. You can invite guests, and they can choose to bring items on the trip. Built using Ruby on Rails, Twitter API, and RSpec."
+    })
   });
-   
   $("#cyclesmart").click(function() {
-    alert("CycleSmart is a cycling-centric app that displays various cycling-related events nearby. It renders accidents involving bicycles, bicycle shops, Citibike stations, and Groupon cycling deals onto a Google map. With CycleSmart, you can also look up cycling directions and save your favorite routes. Built using Ruby on Rails, Javascript, jQuery, Google Maps API, Citibike API, and the Groupon API.")
+    bootbox.dialog({
+      title: "CycleSmart",
+      message: "CycleSmart is a cycling-centric app that displays various cycling-related events nearby. It renders accidents involving bicycles, bicycle shops, Citibike stations, and Groupon cycling deals onto a Google map. With CycleSmart, you can also look up cycling directions and save your favorite routes. Built using Ruby on Rails, Javascript, jQuery, Google Maps API, Citibike API, and the Groupon API."
+    })
   });
   $("#twitterfrags").click(function() {
-    alert("TwttrFrgmnts is an app that helps track Twitter conversation threads. Twitter's normal conversation threads are often a maze of replies that are hard to follow. TwttrFrgmnts makes it easy to follow a Twitter conversation from the original Tweet down to the latest reply. Built using Ruby on Rails, Javascript, jQuery, and D3.")
+    bootbox.dialog({
+      title: "TwttrFrgmnts",
+      message: "TwttrFrgmnts is an app that helps track Twitter conversation threads. Twitter's normal conversation threads are often a maze of replies that are hard to follow. TwttrFrgmnts makes it easy to follow a Twitter conversation from the original Tweet down to the latest reply. Built using Ruby on Rails, Javascript, jQuery, and D3."
+      });
     });
-  })
+})
 
 
 
