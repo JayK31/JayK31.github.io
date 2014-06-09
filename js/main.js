9e49758357338791bd1a13f1c92ae2d68ebf73a7
JayK31 <jay.kaye31@gmail.com>
2014-06-09 19:03:15 -0400

    added smoothstreets

--- js/main.js
+++ js/main.js
@@ -24,6 +24,15 @@ $(document).ready(function() {
       message: "TwttrFrgmnts is an app that helps track Twitter conversation threads. Twitter's normal conversation threads are often a maze of replies that are hard to follow. TwttrFrgmnts makes it easy to follow a Twitter conversation from the original Tweet down to the latest reply. Built using Ruby on Rails, Javascript, jQuery, and D3."
       });
     });
+  $("#smoothstreets").click(function() {
+    bootbox.dialog({
+      title: "SmoothStreets",
+      message: "I bet you've had some awful experiences with poor road quality in the city. What happened? You were in the bike lane. Right were you are supposed to be. Keeping your eyes on the road. And the road came up and wrecked your bike. Or worse.
+            You cursed. You got angry. You might have even gotten seriously hurt.
+            But what did you do about it??? Probably nothing!
+            What if there was an easy, seamless way to seriously, seriously bug your elected officials about bad road conditions AND determine which streets you should avoid?"
+    })
+  })
 })
 
 
