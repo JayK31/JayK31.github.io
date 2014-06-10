9edf9b9716805b0485b3963dbeff83b6ab282ed4
JayK31 <jay.kaye31@gmail.com>
2014-06-09 23:06:10 -0400

    updated projects section

--- js/main.js
+++ js/main.js
@@ -5,36 +5,73 @@ $(document).ready(function() {
   function() {
     $("#me").attr("src", "img/header_image.jpg")
   });
-  
-  $("#tripcollab").click(function() {
-    bootbox.dialog({
-      title: "TripCollaborator",
-      message: "TripCollaborator is a way to plan trips and create a list of items to bring on your trips. You can invite guests, and they can choose to bring items on the trip. Built using Ruby on Rails, Twitter API, and RSpec." 
-    })
-  });
-  $("#cyclesmart").click(function() {
-    bootbox.dialog({
-      title: "CycleSmart",
-      message: "CycleSmart is a cycling-centric app that displays various cycling-related events nearby. It renders accidents involving bicycles, bicycle shops, Citibike stations, and Groupon cycling deals onto a Google map. With CycleSmart, you can also look up cycling directions and save your favorite routes. Built using Ruby on Rails, Javascript, jQuery, Google Maps API, Citibike API, and the Groupon API."
-    })
-  });
-  $("#twitterfrags").click(function() {
-    bootbox.dialog({
-      title: "TwttrFrgmnts",
-      message: "TwttrFrgmnts is an app that helps track Twitter conversation threads. Twitter's normal conversation threads are often a maze of replies that are hard to follow. TwttrFrgmnts makes it easy to follow a Twitter conversation from the original Tweet down to the latest reply. Built using Ruby on Rails, Javascript, jQuery, and D3."
-      })
-    });
-  $("#smoothstreets").click(function() {
-    bootbox.dialog({
-      title: "SmoothStreets",
-      message: "I bet you've had some awful experiences with poor road quality in the city. What happened? You were in the bike lane. Right were you are supposed to be. Keeping your eyes on the road. And the road came up and wrecked your bike. Or worse.
-            You cursed. You got angry. You might have even gotten seriously hurt.
-            But what did you do about it??? Probably nothing!
-            What if there was an easy, seamless way to seriously, seriously bug your elected officials about bad road conditions AND determine which streets you should avoid?"
+
+
+  $(".tripcollaborator").hide();
+  $("#tripcollab-trigger, .tripcollaborator").mouseenter(function() {
+    $(".tripcollaborator").fadeIn();
+  })
+  $("#tripcollab-trigger, .tripcollaborator").mouseleave(function() {
+    $(".tripcollaborator").fadeOut();
+  })
+
+
+  $(".cyclesmart").hide();
+  $("#cyclesmart-trigger, .cyclesmart").mouseenter(function() {
+    $(".cyclesmart").fadeIn();
+  })
+  $("#cyclesmart-trigger, .cyclesmart").mouseleave(function() {
+    $(".cyclesmart").fadeOut();
+  })
+
+  $(".twitterfrags").hide();
+  $("#twitterfrags-trigger, .twitterfrags").mouseenter(function() {
+    $(".twitterfrags").fadeIn();
+  })
+  $("#twitterfrags-trigger, .twitterfrags").mouseleave(function() {
+    $(".twitterfrags").fadeOut();
+  }) 
+
+  $(".smoothstreets").hide();
+  $("#smoothstreets-trigger, .smoothstreets").mouseenter(function() {
+    $(".smoothstreets").fadeIn();
+  })
+  $("#smoothstreets-trigger, .smoothstreets").mouseleave(function() {
+    $(".smoothstreets").fadeOut();
+  })
+});
+
+
+
+  // $("#tripcollab").click(function() {
+  //   bootbox.dialog({
+  //     title: "TripCollaborator",
+  //     message: "TripCollaborator is a way to plan trips and create a list of items to bring on your trips. You can invite guests, and they can choose to bring items on the trip. Built using Ruby on Rails, Twitter API, and RSpec." 
+  //   })
+  // });
+  // $("#cyclesmart").click(function() {
+  //   bootbox.dialog({
+  //     title: "CycleSmart",
+  //     message: "CycleSmart is a cycling-centric app that displays various cycling-related events nearby. It renders accidents involving bicycles, bicycle shops, Citibike stations, and Groupon cycling deals onto a Google map. With CycleSmart, you can also look up cycling directions and save your favorite routes. Built using Ruby on Rails, Javascript, jQuery, Google Maps API, Citibike API, and the Groupon API."
+  //   })
+  // });
+  // $("#twitterfrags").click(function() {
+  //   bootbox.dialog({
+  //     title: "TwttrFrgmnts",
+  //     message: "TwttrFrgmnts is an app that helps track Twitter conversation threads. Twitter's normal conversation threads are often a maze of replies that are hard to follow. TwttrFrgmnts makes it easy to follow a Twitter conversation from the original Tweet down to the latest reply. Built using Ruby on Rails, Javascript, jQuery, and D3."
+  //     })
+  //   });
+  // $("#smoothstreets").click(function() {
+  //   bootbox.dialog({
+  //     title: "SmoothStreets",
+  //     message: "I bet you've had some awful experiences with poor road quality in the city. What happened? You were in the bike lane. Right were you are supposed to be. Keeping your eyes on the road. And the road came up and wrecked your bike. Or worse.
+  //           You cursed. You got angry. You might have even gotten seriously hurt.
+  //           But what did you do about it??? Probably nothing!
+  //           What if there was an easy, seamless way to seriously, seriously bug your elected officials about bad road conditions AND determine which streets you should avoid?"
     
-    })
-  });
-})
+  //   })
+  // });
+
 
 
 
