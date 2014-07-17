39f879cf897e786aaf90e296a26f8d98b42a68ed
JayK31 <jay.kaye31@gmail.com>
2014-07-17 17:54:45 -0400

    favicon and more

--- js/main.js
+++ js/main.js
@@ -4,5 +4,43 @@ $(document).ready(function() {
     controlNav: false,
     slideshowSpeed: 3000
     });
+
+  $('.custom-button').click(function(event) {
+    event.preventDefault();
+    $('html,body').animate({ 
+      scrollTop: $('#about').offset().top 
+    }, 2000);
+  });
+
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 600){
+        $(".navbar")
+            .fadeIn('slow')
+            .css("display", "inline")
+            .css("position", "fixed")
+            .css("opacity", 0.9)
+    }
+  });
+  $(window).scroll(function(){
+    if($(window).scrollTop() <= 600){
+        $(".navbar").fadeOut("fast");
+    }
+  });
+
+  $('#about-me-navbar').click(function(event) {
+    event.preventDefault();
+    $('html,body').animate({ 
+      scrollTop: $('#about').offset().top 
+    }, 2000);
+
+  });
+
+  $('#projects-navbar').click(function(event) {
+    event.preventDefault();
+    $('html,body').animate({ 
+      scrollTop: $('#projects').offset().top 
+    }, 2000);
+  });
+
 });
 
